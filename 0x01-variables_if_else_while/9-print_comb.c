29a5e05ae8057aa326007714bfd6296e9a653bbc
Boitumelo-08 <khananiboitu@gmail.com>
2023-06-18 19:08:32 -0700

    Possible combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Prints all possible combinations of single digit numbers
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+	putchar(d);
+	if (d != '9')
+	{
+	putchar(',');
+	putcahr(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
