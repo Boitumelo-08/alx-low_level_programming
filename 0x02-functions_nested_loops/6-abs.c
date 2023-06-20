802d7a3dd9b8a1864243323b9510b31ada003fd9
Boitumelo-08 <khananiboitu@gmail.com>
2023-06-20 15:29:19 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "nain.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: Integer computed
+ * Return: Absoolute vaue of an integer
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
