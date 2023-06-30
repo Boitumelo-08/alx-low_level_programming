eacd1892879bc636fd3cb6207a589cce71ffcc92
Boitumelo-08 <khananiboitu@gmail.com>
2023-06-30 04:16:18 -0700

    Capitilization

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @c: input string
+ * Return: pointer to dest
+ */
+
+char *cap_string(char *c)
+{
+	int index = 0;
+
+	while (c[index])
+	{
+	while (!(c[index] >= 'a' && c[index] <= 'z'))
+	index++;
+	if (c[index - 1] == ' ' ||
+	c[index - 1] == '\t' ||
+	c[index - 1] == '\n' ||
+	c[index - 1] == ',' ||
+	c[index - 1] == ';' ||
+	c[index - 1] == '.' ||
+	c[index - 1] == '!' ||
+	c[index - 1] == '?' ||
+	c[index - 1] == '"' ||
+	c[index - 1] == '(' ||
+	c[index - 1] == ')' ||
+	c[index - 1] == '{' ||
+	c[index - 1] == '}' ||
+	index == 0)
+	c[index] -= 32;
+	index++;
+	}
+	return (c);
+}
