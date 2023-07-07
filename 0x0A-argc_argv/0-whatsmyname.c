13d387a20bd4a86fd22cc30262950efd9c108adf
Boitumelo-08 <khananiboitu@gmail.com>
2023-07-07 06:13:03 -0700

    Print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * argc: counts the number of parameters
+ * argv: pointer array
+ * Return: Always success
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc > 0)
+		print("%s\n", argv[0]);
+	return (0);
+}
