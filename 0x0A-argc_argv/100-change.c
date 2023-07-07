8b5c41497813d275ff3f0ea2ebc7d9e4ebd4dd9f
Boitumelo-08 <khananiboitu@gmail.com>
2023-07-07 07:16:17 -0700

    Change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ * @argc: the size of the argv array
+ * @argv: the array
+ * Return: 1 or 0
+ */
+int main(int argc, char **argv)
+{
+	int i, amount, coins = 0;
+	int values[] = {25, 10, 5, 2, 1};
+
+	if (argc - 1 != 1)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	amount = atoi(argv[1]);
+	for (i = 0; i < 5; i++)
+		while (values[i] <= amount)
+		{
+			coins++;
+			amount -= values[i];
+		}
+	printf("%i\n", coins);
+	return (0);
+}
