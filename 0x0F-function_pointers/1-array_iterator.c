a20c8326279b747550f5d681d1a356767fb61fdc
Boitumelo-08 <khananiboitu@gmail.com>
2023-07-19 07:32:19 -0700

    Array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,9 +10,9 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int c;
+	unsigned int i;
 
 	if (array && action)
-		for (c = 0; c < size; c++)
-			action(array[c]);
+		for (i = 0; i < size; i++)
+			action(array[i]);
 }
